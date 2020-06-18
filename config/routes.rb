080fe19914744f98cf9eb5dc32c8fddc0cc9294e
shoynoi <akira.yoshino@luxiar.com>
2020-06-18 10:01:21 +0900

    commentのルーティング定義を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'react_crud_data#index'
-  resources :react_crud_data, except: [:edit]
+  resources :react_crud_data, except: [:edit] do
+    resources :comments, only: %i[index create update destroy]
+  end
 end
